2878c64c32e2a7fe3e04e647018f2d54049c4282
victorhartung <victor_hartung@hotmail.com>
2024-01-18 13:04:58 -0300

    upd entities

--- src/main/java/com/webservice/course/config/TestConfig.java
+++ src/main/java/com/webservice/course/config/TestConfig.java
@@ -1,9 +1,13 @@
 package com.webservice.course.config;
 
+import com.webservice.course.entities.Category;
 import com.webservice.course.entities.Order;
+import com.webservice.course.entities.Product;
 import com.webservice.course.entities.User;
 import com.webservice.course.entities.enums.OrderStatus;
+import com.webservice.course.repositories.CategoryRepository;
 import com.webservice.course.repositories.OrderRepository;
+import com.webservice.course.repositories.ProductRepository;
 import com.webservice.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -23,6 +27,11 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -33,8 +42,30 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.PAID);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
     }
 }
--- src/main/java/com/webservice/course/entities/Category.java
+++ src/main/java/com/webservice/course/entities/Category.java
@@ -1,10 +1,13 @@
 package com.webservice.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -17,6 +20,9 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
     public Category() {
 
     }
@@ -55,5 +61,8 @@ public class Category implements Serializable {
         return Objects.hash(id);
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
 
 }
